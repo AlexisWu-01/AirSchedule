c249f5d4fb2f0f295ec31629549fd1e4443c5373
Alexis_Xinyi_Wu <wuxinyi2000@gmail.com>
2024-10-14 11:27:58 -0700

    test plist

--- AirSchedule/APIKeys.swift
+++ AirSchedule/APIKeys.swift
@@ -5,7 +5,23 @@
 //  Created by Xinyi WU on 10/14/24.
 //
 
+import Foundation
+
 struct APIKeys {
-    static let serpAPIKey = "YOUR_SERP_API_KEY_HERE"
-    static let openAIAPIKey = "YOUR_OPENAI_API_KEY_HERE"
+    static func value(for key: String) -> String {
+        guard let path = Bundle.main.path(forResource: "keys", ofType: "plist"),
+              let dict = NSDictionary(contentsOfFile: path) as? [String: Any],
+              let value = dict[key] as? String else {
+            fatalError("Could not find key \(key) in keys.plist")
+        }
+        return value
+    }
+    
+    static var serpAPIKey: String {
+        return value(for: "SERP_API_KEY")
+    }
+    
+    static var openAIAPIKey: String {
+        return value(for: "OPENAI_API_KEY")
+    }
 }
